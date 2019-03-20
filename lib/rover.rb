a3ef0f34167be65dfd7ad153a9783e5efe30427e
Student <student@makerss-mbp.home>
2019-03-20 23:26:41 +0000

    Added user stories and initialize method with testing

--- lib/rover.rb
+++ lib/rover.rb
@@ -1,3 +1,10 @@
-def test
-  true
+class Rover
+
+  attr_accessor :position
+  def initialize(x, y, direction)
+    @position = [x, y, direction]
+    @x_axis = x
+    @y_axis = y
+    @direction = direction
+  end
 end
--- spec/rover_spec.rb
+++ spec/rover_spec.rb
@@ -1,7 +1,8 @@
 require_relative '../lib/rover'
 
 describe RSpec do
-  it "tests for true" do
-    expect(test).to eq(true)
+  it "initialises rover at default value facing North" do
+    rover = Rover.new(0, 0, "N")
+    expect(rover.position).to eq([0, 0, "N"])
   end
 end
