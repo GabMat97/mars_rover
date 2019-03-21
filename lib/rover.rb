8bfe7e7211f6d030d3fcbc82b24e30022d624d1c
Student <student@Makerss-MacBook-Pro.local>
2019-03-21 13:56:46 +0000

    Added tests for move method according to direction

--- lib/rover.rb
+++ lib/rover.rb
@@ -68,5 +68,4 @@ class Rover
       @position.push("N")
     end
   end
-
 end
--- spec/rover_spec.rb
+++ spec/rover_spec.rb
@@ -71,4 +71,23 @@ describe RSpec do
     expect(rover.turn_left).to eq([0, 0, "N"])
   end
 
+  it "tests for move when facing North" do
+    rover = Rover.new(0, 0, "N")
+    expect(rover.move).to eq([0, 1, "N"])
+  end
+
+  it "tests for move when facing East" do
+    rover = Rover.new(0, 0, "E")
+    expect(rover.move).to eq([1, 0, "E"])
+  end
+
+  it "tests for move when facing South" do
+    rover = Rover.new(0, 1, "S")
+    expect(rover.move).to eq([0, 0, "S"])
+  end
+
+  it "tests for move when facing West" do
+    rover = Rover.new(1, 0, "W")
+    expect(rover.move).to eq([0, 0, "W"])
+  end
 end
