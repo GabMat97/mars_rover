b5f9e76c84d10e969c2cf5ef04591f096627db4b
Student <student@makerss-mbp.home>
2019-03-21 00:08:12 +0000

    Added code to pass tests and corrected mistakes

--- lib/rover.rb
+++ lib/rover.rb
@@ -2,9 +2,25 @@ class Rover
 
   attr_accessor :position
   def initialize(x, y, direction)
-    @position = [x, y, direction]
     @x_axis = x
     @y_axis = y
     @direction = direction
+    update_position
+
+  end
+  def update_position
+    @position =  [@x_axis, @y_axis, @direction]
+  end
+
+  def increase_x
+    @x_axis = @x_axis + 1
+    update_position
+    return @position
+  end
+
+  def decrease_x
+    @x_axis = @x_axis - 1
+    update_position
+    return @position
   end
 end
