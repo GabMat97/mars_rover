898d0292a159ec9cc88debf15c5f6f7fa924001f
Student <student@makerss-mbp.home>
2019-03-21 00:15:21 +0000

    Added tests for y changes

--- spec/rover_spec.rb
+++ spec/rover_spec.rb
@@ -10,8 +10,19 @@ describe RSpec do
     rover = Rover.new(0, 0, "N")
     expect(rover.increase_x).to eq([1, 0, "N"])
   end
+
   it "tests for x going down by one" do
     rover = Rover.new(1, 0, "N")
-    expect(rover.decrease_x).to eq([0, 0, "N"]) #will add logic of direction later 
+    expect(rover.decrease_x).to eq([0, 0, "N"]) #will add logic of direction later
+  end
+
+  it "tests for y going up by one" do
+    rover = Rover.new(0, 0, "N")
+    expect(rover.increase_y).to eq([0, 1, "N"])
+  end
+
+  it "tests for y going down by one" do
+    rover = Rover.new(0, 1, "N")
+    expect(rover.decrease_y).to eq([0, 0, "N"])
   end
 end
