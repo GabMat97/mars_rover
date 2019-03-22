4519265de193f52730e20a8f34034c5127ee6405
Student <student@makerss-mbp.home>
2019-03-22 11:43:02 +0000

    Added map class with test

---
+++ lib/map.rb
@@ -0,0 +1,7 @@
+class Map
+  attr_accessor :x_limit, :y_limit
+  def initialize(x, y)
+    @x_limit = x
+    @y_limit = y
+  end
+end
---
+++ spec/map_spec.rb
@@ -0,0 +1,6 @@
+require_relative '../lib/map'
+describe RSpec do
+  it "initializes map with coordinates" do
+    map = Map.new(5, 5)
+  end
+end
